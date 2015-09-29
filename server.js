9a214dbe590f706c9d167e574ade80a19a5f577b
Chen Ze <chenzex@ccs.neu.edu>
2015-09-28 23:56:03 -0400

    'assignment2'

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set("views", __dirname + '\\public')
 app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    // res.sendfile('hello.html',{ root: __dirname + "/public" });
+    res.render('hello');
+});
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
